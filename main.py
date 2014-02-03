99d93cda8693a55a27a061101fa603a35c969fed
jackieqif <jackieqi@gmail.com>
2014-02-03 12:11:53 -0800

    app engine file added

---
+++ main.py
@@ -0,0 +1,37 @@
+import webapp2
+import jinja2
+from google.appengine.api import users
+
+
+def Render(name, data=None):
+  if not data:
+    data = {}
+  environment = jinja2.Environment(
+      loader=jinja2.FileSystemLoader(['template']))
+  environment.globals.update({
+      'uri_for': webapp2.uri_for,
+      'request': webapp2.get_request()
+      })
+  template = environment.get_or_select_template(name)
+  result = template.render(data)
+  return result
+
+
+class MainHandler(webapp2.RequestHandler):
+  """Handle index page"""
+  def get(self):
+    user = users.get_current_user()
+    username = user.nickname()
+    sender = self.request.get('sender')
+    data = {'sender': sender, 'username': username}
+    result = Render('index.template', data)
+    self.response.write(result)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+    ], debug=True)
+
+
+if __name__ == '__main__':
+  main()
